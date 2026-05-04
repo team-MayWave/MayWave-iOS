a85f42114eafa1ced2b9b0b3f36af55624a81a57
김준표juunpy0 <junpyokim0106@gmail.com>
2026-05-05 00:13:32 +0900

    init

---
+++ Maywave-iOS/Maywave_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maywave_iOSApp.swift
+//  Maywave-iOS
+//
+//  Created by 김준표 on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Maywave_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
